b1091b3629adeae4fe6277f13446234879ad2034
MAddyg1 <68590827+MAddyg1@users.noreply.github.com>
2021-07-08 13:42:50 +0530

    hand animations test

---
+++ Shader Graph/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator hand;
+    public Animator gun;
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hand.SetTrigger("isFire");
+            gun.SetTrigger("isFire");
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            hand.SetTrigger("isReload");
+            gun.SetTrigger("isReload");
+        }
+    }
+}
