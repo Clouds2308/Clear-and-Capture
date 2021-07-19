d6119b9cbdf37590044b0d6f60816617b44bbf79
MAddyg1 <68590827+MAddyg1@users.noreply.github.com>
2021-07-19 12:01:38 +0530

    weapon bob almost done

--- Shader Graph/Assets/Scripts/Weapon/WeaponBob.cs
+++ Shader Graph/Assets/Scripts/Weapon/WeaponBob.cs
@@ -3,45 +3,44 @@
 public class WeaponBob : MonoBehaviour
 {
     
-    [SerializeField] private PlayerInput _input;
-    [SerializeField] private PlayerMovement _movement;
+    [SerializeField] private Transform WeaponParent;
+    private PlayerInput _input;
+    private PlayerMovement _movement;
 
     private Vector3 _weaponParentOrigin;
     private Vector3 _targetWeaponBobPosition;
     private float _idleCounter = Mathf.PI / 2;
     private float _moveCounter = Mathf.PI / 2;
+    private float _bobSpeed;
 
-    public float xMultiplier;
-    public float yMultiplier;
     
-    public Transform WeaponParent;
-    
-    void BobWeapon(float p_z,float p_x_intensity,float p_y_intensity)
+    void BobWeapon(float p_z,float p_y_intensity)
     {
-        _targetWeaponBobPosition = _weaponParentOrigin + new Vector3(Mathf.Cos(p_z * xMultiplier) * p_x_intensity, Mathf.Sin(p_z * yMultiplier) * p_y_intensity,0);
+        _targetWeaponBobPosition = _weaponParentOrigin + new Vector3(0,Mathf.Sin(p_z * _bobSpeed) * p_y_intensity, 0);
     }
 
     private void Start()
     {
         _weaponParentOrigin = WeaponParent.localPosition;
+        _input = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        _movement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (_input.InputX == 0 && _input.InputZ == 0)
         {
-            BobWeapon(_idleCounter,0.001f,0.005f);
+            _bobSpeed = 3f;
+            BobWeapon(_idleCounter, 0.005f);
             _idleCounter += Time.deltaTime;
-            WeaponParent.localPosition = Vector3.Lerp(WeaponParent.localPosition, _targetWeaponBobPosition, Time.deltaTime * 2f);
+            WeaponParent.localPosition = Vector3.Lerp(WeaponParent.localPosition, _targetWeaponBobPosition, Time.deltaTime);
         }
         else
         {
-            if (_movement.IsGrounded)
-            {
-                BobWeapon(_moveCounter, 0.05f, 0.007f);
-                _moveCounter += Time.deltaTime;
-                WeaponParent.localPosition = Vector3.Lerp(WeaponParent.localPosition, _targetWeaponBobPosition, Time.deltaTime * 10f);
-            }
+            _bobSpeed = 9f;
+            BobWeapon(_moveCounter, 0.01f);
+            _moveCounter += Time.deltaTime;
+            WeaponParent.localPosition = Vector3.Lerp(WeaponParent.localPosition, _targetWeaponBobPosition, Time.deltaTime * 5f);            
         }
 
         if (_idleCounter > Mathf.PI * 2) _idleCounter = 0;
