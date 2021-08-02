35fbc2a5a9f507d6f30e3d6aac671031e7e87abc
MAddyg1 <68590827+MAddyg1@users.noreply.github.com>
2021-08-02 13:26:05 +0530

    swat patrol

---
+++ Shader Graph/Assets/PatrolSwat.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolSwat : MonoBehaviour
+{
+    [SerializeField] private Transform[] _points;
+    private int _destPoint;
+    private NavMeshAgent _swatAgent;
+
+    private void Start()
+    {
+        _swatAgent = GetComponent<NavMeshAgent>();
+
+        GotoNextPoint();
+    }
+
+    private void GotoNextPoint()
+    {
+        if (_points.Length == 0)
+            return;
+
+        _swatAgent.destination = _points[_destPoint].position;
+
+        _destPoint = (_destPoint + 1) % _points.Length;
+    }
+
+    private void Update()
+    {
+        if(!_swatAgent.pathPending && _swatAgent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+}
