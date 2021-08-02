b0cce80b70a47264ceb9e02c06a319039450f122
MAddyg1 <68590827+MAddyg1@users.noreply.github.com>
2021-08-02 18:22:10 +0530

    ye

---
+++ Shader Graph/Assets/Scripts/Tutorial/WeaponSwitch.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject _m1911UIPanel;
+    [SerializeField] private GameObject _f1UIPanel;
+    [SerializeField] private TextMeshProUGUI _currBulletsText;
+    [SerializeField] private TextMeshProUGUI _maxBulletsText;
+
+    public Weapon[] _weapon;
+    public static int SelectedWeapon = 0;
+
+    private void Start()
+    {
+        SelectWeapon();
+    }
+
+    private void Update()
+    {
+        int previousselectedweapon = SelectedWeapon;
+
+        if (SelectedWeapon == 0)
+        {
+            _currBulletsText.text = _weapon[0].BulletInMag.ToString();
+            _maxBulletsText.text = _weapon[0].MaxBulletsInMag.ToString();
+        }
+
+        if (SelectedWeapon == 1)
+        {
+            _currBulletsText.text = _weapon[1].BulletInMag.ToString();
+            _maxBulletsText.text = _weapon[1].MaxBulletsInMag.ToString();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SelectedWeapon = 0;
+            _f1UIPanel.SetActive(false);
+            _m1911UIPanel.SetActive(true);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >=2)
+        {
+            SelectedWeapon = 1;
+            _m1911UIPanel.SetActive(false);
+            _f1UIPanel.SetActive(true);
+        }
+
+        if(previousselectedweapon!=SelectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        int i = 0;
+
+        foreach(Transform weapon in transform)
+        {
+            if (i == SelectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
