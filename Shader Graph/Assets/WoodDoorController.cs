2db5add84f8d4bb974b0c2e6afbfe167bb569305
MAddyg1 <68590827+MAddyg1@users.noreply.github.com>
2021-08-24 16:00:24 +0530

    f

---
+++ Shader Graph/Assets/WoodDoorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WoodDoorController : MonoBehaviour
+{
+    private Animator _woodDoorAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                _woodDoorAnimator.SetTrigger("IsOpen");
+            }
+        }
+    }
+}
